5f4361e3e1216aa21a3804f3543d58bcb9bd68ae
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-10-31 15:50:08 +0530

    chore: added rich-text serialization

--- src/payload/blocks/Form/Components/Fields/Message.tsx
+++ src/payload/blocks/Form/Components/Fields/Message.tsx
@@ -1,19 +1,42 @@
-import { getHTML } from '@/utils/slateToHTML'
+import RichText from '@/components/RichText'
 
 interface MessageField {
-  message?:
-    | {
+  message?: {
+    root: {
+      type: string
+      children: {
+        type: string
+        version: number
         [k: string]: unknown
       }[]
-    | null
+      direction: ('ltr' | 'rtl') | null
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | ''
+      indent: number
+      version: number
+    }
+    [k: string]: unknown
+  } | null
   id?: string | null
   blockName?: string | null
   blockType: 'message'
 }
 const Message: React.FC<MessageField> = ({ message }) => {
   return (
-    <div className=' prose max-w-none! text-justify text-base-content/80 md:prose-xl prose-h1:text-primary-content prose-h2:text-primary-content prose-h3:text-primary-content prose-h4:text-primary-content prose-h5:text-primary-content prose-h6:text-primary-content  [&_iframe]:aspect-video [&_iframe]:w-full [&_iframe]:rounded'>
-      <div dangerouslySetInnerHTML={{ __html: getHTML(message!) }} />
+    <div className=' prose max-w-none! text-base-content/80 md:prose-xl prose-h1:text-primary-content prose-h2:text-primary-content prose-h3:text-primary-content prose-h4:text-primary-content prose-h5:text-primary-content prose-h6:text-primary-content text-justify  [&_iframe]:aspect-video [&_iframe]:w-full [&_iframe]:rounded'>
+      <RichText
+        data={
+          message ?? {
+            root: {
+              type: 'root',
+              children: [],
+              direction: null,
+              format: '',
+              indent: 0,
+              version: 1,
+            },
+          }
+        }
+      />
     </div>
   )
 }
